d98febd8505fb038abf7546e48d9e63594f747ab
s1mple-User <u3905173@gmail.com>
2025-07-05 10:03:34 -0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import serverlessExpress from '@vendia/serverless-express';
+
+let server: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const expressApp = express();
+  const adapter = new ExpressAdapter(expressApp);
+  const app = await NestFactory.create(AppModule, adapter);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(parseInt(process.env.PORT) || 8000);
+  await app.init();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler = async (...args: any[]) => {
+  if (!server) {
+    server = await bootstrap();
+  }
+  return server(...args);
+};
